9fe5e325a76d3ed60b25b1ad51dac2f80d7c9b89
darshoned <34932380+darshoned@users.noreply.github.com>
2025-01-20 00:17:59 +0800

    Update output_data.py

--- output_data.py
+++ output_data.py
@@ -1,97 +1,251 @@
 import math
 import pandas as pd
 
-def Calculate_Layout_outputs(s1, s2, live_load, column_size, structure_type, length, width,b_s1_mm, d_s1_mm,b_s2_mm, d_s2_mm, slab_thickness):
+def calculate_layout_outputs(s1, s2, live_load, column_size_mm, selected_column, selected_beam, selected_slab, length, width, b_s1_mm, d_s1_mm,b_s2_mm, d_s2_mm, slab_thickness_mm, slab_max_spacing_pt_mm, hcs_slab_thickness_mm):
     building_height = 6
     
+
+    # Initialize all return-related variables to default values
+    no_volumetric_pc_com = 0
+    no_vertical_pc_com = 0
+    no_propped_slabs = 0
+    no_unpropped_slabs = 0
+    formwork_area_slabs = 0
+    formwork_area_beams = 0
+    no_column_formworks = 0
+    weight_loose_rebar = 0
+    weight_slab_rebar = 0
+    cis_volume = 0
+    no_tower_cranes = 0
+    no_concrete_pumps = 1
+    no_construction_hoists = 0
+    no_passenger_material_hoists = 1
+    no_gondolas = 0
+    no_mewps = 0
+    hoist_count_passenger_material = 0
+    hoist_count_tower_crane = 0
+    no_concrete_truck_deliveries = 0
+    no_trailer_deliveries = 0
+    beam_manhours = 0
+    slab_manhours = 0
+    column_manhours = 0
+    casting_manhours = 0
+    total_mandays_crane = 0
+    total_productivity_crane = 0
+    slab_cis_volume = 0
+    beam_cis_volume = 0
+    column_cis_volume = 0
+    weight_beam_rebar = 0
+    weight_column_rebar = 0
+    weight_slab_rebar = 0
+    weight_loose_rebar = 0
+    beam_hoist_count_tower_crane = 0
+    slab_hoist_count_tower_crane = 0
+    column_hoist_count_tower_crane = 0
+
     file_name = "productivity_list.csv"
     df = pd.read_csv(file_name)
-
-    if structure_type == "CIS Beam + CIS Slab + CIS Column":
     
-        no_s1 = (length/s1) * ((width/s2) + 1)
-        no_s2 = ((length/s1)+1) * ((width/s2))
-        no_column = ((length/s1)+1) * ((width/s2) + 1)
+    # Export manhour values for each category
+    manhour_vertical_nonrc_pc = df.loc[df['category'] == "vertical_nonrc_pc", 'manhour'].iloc[0]
+    manhour_vertical_beamfw_pc = df.loc[df['category'] == "vertical_beamfw_pc", 'manhour'].iloc[0]
+    manhour_casting_pump_m3 = df.loc[df['category'] == "casting_pump_m3", 'manhour'].iloc[0]
+    manhour_loosebar_ton = df.loc[df['category'] == "loosebar_ton", 'manhour'].iloc[0]
+    manhour_mesh_ton = df.loc[df['category'] == "mesh_ton", 'manhour'].iloc[0]
+    manhour_scaffold_m3 = df.loc[df['category'] == "scaffold_m3", 'manhour'].iloc[0]
+    manhour_posttension_pc = df.loc[df['category'] == "posttension_pc", 'manhour'].iloc[0]
+    
+    no_s1 = (length/s1) * ((width/s2) + 1)
+    no_s2 = ((length/s1) + 1) * ((width/s2))
+    no_column = ((length/s1) + 1) * ((width/s2) + 1)
+    
+    b_s1_m = b_s1_mm / 1000
+    d_s1_m = d_s1_mm / 1000
+    b_s2_m = b_s2_mm / 1000
+    d_s2_m = d_s2_mm / 1000
+    slab_thickness_m = slab_thickness_mm / 1000
+    column_size_m = column_size_mm / 1000
+    
+    # assign values to equipment data
+    if length <= 30:
+        no_tower_cranes = 1
+    elif 30 < length <= 70:
+        no_tower_cranes = 2
+    elif 70 < length <= 100:
+        no_tower_cranes = 3
+    
+
+    
+    if (selected_beam == "CIS Beam"):
+        formwork_area_beams = (no_s1 * s1 * (b_s1_m + 1)) + (no_s2 * s2 * (b_s2_m + 1))
+        beam_rebar_percentage = 4
+        weight_beam_rebar = math.ceil(((no_s1 * b_s1_m * d_s1_m * s1) + (no_s2 * b_s2_m * d_s2_m * s2)) * 7.85 * beam_rebar_percentage/100) #7.85 density rebar
+        beam_cis_volume = math.ceil((no_s1 * b_s1_m * d_s1_m * s1) + (no_s2 * b_s2_m * d_s2_m * s2))
+
+        beam_manhours = math.ceil(weight_beam_rebar * manhour_loosebar_ton + formwork_area_beams /16 * manhour_vertical_beamfw_pc + formwork_area_beams * building_height * 0.5 * manhour_scaffold_m3) #horizontal formwork 16m2 a pc
         
-    # Assign values to design data
-        no_volumetric_pc_com = 0
-        no_vertical_pc_com = 0
-        no_propped_slabs = 4
-        no_unpropped_slabs = 0
-        formwork_area_beams = (no_s1*s1*((b_s1_mm/1000)+1)) + (no_s2*s2*((b_s2_mm/1000)+1))
-        formwork_area_slabs = length * width - (formwork_area_beams/2)
+        beam_hoist_count_tower_crane = math.ceil(weight_beam_rebar/6 + formwork_area_beams /16) 
+        
+    if (selected_beam == "PT Beam"):
+        formwork_area_beams = (no_s1 * s1 * (b_s1_m + 1)) + (no_s2 * s2 * (b_s2_m + 1))
+        beam_rebar_percentage = 4
+        weight_beam_rebar = math.ceil(((no_s1 * b_s1_m * d_s1_m * s1) + (no_s2 * b_s2_m * d_s2_m * s2)) * 7.85 * beam_rebar_percentage/100) #7.85 density rebar
+        beam_cis_volume = math.ceil((no_s1 * b_s1_m * d_s1_m * s1) + (no_s2 * b_s2_m * d_s2_m * s2))
+        total_no_pt_tendon = int(length / s1 + 1 + width / s2 + 1)
+
+        beam_manhours = math.ceil(total_no_pt_tendon * manhour_posttension_pc + weight_beam_rebar * manhour_loosebar_ton + formwork_area_beams /16 * manhour_vertical_beamfw_pc + formwork_area_beams * building_height * 0.5 * manhour_scaffold_m3) #horizontal formwork 16m2 a pc
+        
+        beam_hoist_count_tower_crane = math.ceil(weight_beam_rebar/6 + formwork_area_beams /16) 
+        
+    if (selected_column == "CIS Column"):
         no_column_formworks = no_column
-        weight_beam_rebar = math.ceil(((no_s1 * (b_s1_mm/1000) * (d_s1_mm/1000) * s1) + (no_s2 * (b_s2_mm/1000) * (d_s2_mm/1000) * s2)) * 0.353) #(factor rc to rebar 3% tones)
-        weight_slab_rebar = math.ceil((formwork_area_slabs * slab_thickness) * 0.235) #(factor rc to rebar 2% tones)
-        cis_volume = math.ceil((no_s1 * (b_s1_mm/1000) * (d_s1_mm/1000) * s1) + ((no_s2 * (b_s2_mm/1000) * (d_s2_mm/1000) * s2)) + (no_column * column_size * column_size * building_height) + (formwork_area_slabs * slab_thickness))
-
-        # assign values to equipment data
-        if length <= 30:
-            no_tower_cranes = 1
-        elif 30 < length <= 70:
-            no_tower_cranes = 2
-        elif 70 < length <= 100:
-            no_tower_cranes = 3
+        column_cis_volume = math.ceil((no_column * column_size_m * column_size_m * building_height))
+        column_rebar_percentage = 2
+        weight_column_rebar = math.ceil((no_column * column_size_m * column_size_m * building_height) * 7.85 * column_rebar_percentage/100) #7.85 density rebar
         
-        no_concrete_pumps = 1
-        no_passenger_material_hoists = 1
-        no_construction_hoists = 0
-        no_gondolas = 0
-        no_mewps = 0
-
-        # assign values to utility data
-        hoist_count_passenger_material = 0
-        hoist_count_tower_crane = math.ceil((no_column * 2) + (weight_beam_rebar+weight_slab_rebar)/15 + (formwork_area_beams + formwork_area_slabs)/36)
-        no_concrete_truck_deliveries = math.ceil(cis_volume / 8.5)
-        no_trailer_deliveries = math.ceil((no_column / 5) + (weight_beam_rebar+weight_slab_rebar)/15)
-
-        beam_manhours = math.ceil(weight_beam_rebar * df.loc[df['category'] == "loosebar_ton", 'manhour'].iloc[0] + formwork_area_beams /16 * df.loc[df['category'] == "vertical_beamfw_pc", 'manhour'].iloc[0] + formwork_area_beams * building_height * 0.5 * df.loc[df['category'] == "scaffold_m3", 'manhour'].iloc[0])
-        slab_manhours = math.ceil(weight_slab_rebar * df.loc[df['category'] == "mesh_ton", 'manhour'].iloc[0] + formwork_area_slabs / 16 * df.loc[df['category'] == "vertical_nonrc_pc", 'manhour'].iloc[0] + formwork_area_slabs * building_height * 0.5 * df.loc[df['category'] == "scaffold_m3", 'manhour'].iloc[0])
-        column_manhours = math.ceil(no_column * df.loc[df['category'] == "vertical_nonrc_pc", 'manhour'].iloc[0] * 2)
-        casting_manhours = math.ceil(cis_volume * df.loc[df['category'] == "casting_pump_m3", 'manhour'].iloc[0])
-
-        # assign values to manpower data
-        total_mandays_crane = ((beam_manhours + slab_manhours + column_manhours + casting_manhours)*1.3 +240 + 1800)/ 8 #weather 1.1 manday 8hr safety 1.1 staircase 240 ramp 1400
-        total_productivity_crane = ( (length * width) +600) / total_mandays_crane # ramp 600m2
-
+        column_manhours = math.ceil(no_column * manhour_vertical_nonrc_pc + weight_column_rebar * manhour_loosebar_ton)
+        
+        column_hoist_count_tower_crane = math.ceil(no_column + weight_column_rebar/6) 
+        
+    if (selected_column == "PC Column"):
+        no_vertical_pc_com = no_column
+        column_cis_volume = math.ceil((no_column * column_size_m * column_size_m * building_height))
+        column_rebar_percentage = 2
+        weight_column_rebar = math.ceil((no_column * column_size_m * column_size_m * building_height) * 7.85 * column_rebar_percentage/100) #7.85 density rebar
+        
+        column_manhours = math.ceil(no_column * manhour_vertical_nonrc_pc)
+        
+        column_hoist_count_tower_crane = math.ceil(no_column) 
     
-        # Design Outputs
-        design_output = [
-        round(no_volumetric_pc_com),
-        round(no_vertical_pc_com),
-        round(no_propped_slabs),
-        round(no_unpropped_slabs),
-        round(formwork_area_slabs),
-        round(formwork_area_beams),
-        round(no_column_formworks),
-        round(weight_beam_rebar),
-        round(weight_slab_rebar),
-        round(cis_volume)
-        ]
+    if (selected_slab == "CIS Slab"):
+        formwork_area_slabs = length * width - (formwork_area_beams / 2)
+        slab_rebar_percentage = 1.5
+        weight_slab_rebar = math.ceil((formwork_area_slabs * slab_thickness_m) * 7.85 * slab_rebar_percentage/100)
+        slab_cis_volume = formwork_area_slabs * slab_thickness_m
+        
+        slab_manhours = math.ceil(weight_slab_rebar * manhour_mesh_ton + formwork_area_slabs / 16 * manhour_vertical_beamfw_pc + formwork_area_slabs * building_height * 0.5 * manhour_scaffold_m3) #5 storey scaffold install about 2.5floors
 
-        # Equipment Outputs
-        equipment_output = [
-        round(no_tower_cranes),
-        round(no_concrete_pumps),
-        round(no_construction_hoists),
-        round(no_passenger_material_hoists),
-        round(no_gondolas),
-        round(no_mewps)
-        ]
+        slab_hoist_count_tower_crane = math.ceil(weight_slab_rebar /6 + formwork_area_slabs/16) 
+        
+    if (selected_slab == "PT Flat Slab"):
+        formwork_area_slabs = length * width
+        slab_rebar_percentage = 4
+        weight_slab_rebar = math.ceil((formwork_area_slabs * slab_thickness_m) * 7.85 * slab_rebar_percentage/100)
+        slab_cis_volume = formwork_area_slabs * slab_thickness_m
+        
+        # Calculate tendon spacing (use the average of 6h to 8h)
+        tendon_spacing = 6 * slab_thickness_m  # Average spacing in meters
 
-        # Utility Outputs
-        utility_output = [
-        round(hoist_count_passenger_material),
-        round(hoist_count_tower_crane),
-        round(no_concrete_truck_deliveries),
-        round(no_trailer_deliveries)
-        ]
+        # Number of tendons in each direction
+        no_pt_tendon_x = math.ceil(length / tendon_spacing)  # Tendons in x-direction
+        no_pt_tendon_y = math.ceil(width / tendon_spacing)   # Tendons in y-direction
 
-     # Manpower Outputs
-        manpower_output = [
-        round(total_mandays_crane),
-        round(total_productivity_crane,3),
-        round(total_productivity_crane/2,3)
-        ]
+        # Total tendons
+        total_no_pt_tendon = no_pt_tendon_x + no_pt_tendon_y 
+        
+        slab_manhours = math.ceil(total_no_pt_tendon * manhour_posttension_pc + weight_slab_rebar * manhour_mesh_ton + formwork_area_slabs / 16 * manhour_vertical_beamfw_pc + formwork_area_slabs * building_height * 0.5 * manhour_scaffold_m3) #5 storey scaffold install about 2.5floors
+    
+        slab_hoist_count_tower_crane = math.ceil(weight_slab_rebar /6 + formwork_area_slabs/16) 
+    
+    if (selected_slab == "1.2HC Slab"):
+        topping_area_slabs = length * width - (formwork_area_beams / 2)
+        formwork_area_slabs = 0
+        slab_rebar_percentage = 1.5
+        weight_slab_rebar = math.ceil((formwork_area_slabs * slab_thickness_m) * 7.85 * slab_rebar_percentage/100)
+        slab_cis_volume = topping_area_slabs * slab_thickness_m
+        
+        # Calculate the number of x positions
+        N_x = int((length + s1 - (s1 * 1.5)) / s1)
+
+        # Calculate the number of y positions
+        N_y = int((width + s2 - (s2 + 2 * column_size_mm)) / s2)
+
+        # Calculate the number of stacked rectangles per position
+        N_stacked = math.floor(s2 / 1.2)
+
+        # Calculate the total number of 1.2HCS
+        no_unpropped_slabs = int(N_x * N_y * N_stacked)
+ 
+        slab_manhours = math.ceil(weight_slab_rebar * manhour_mesh_ton + no_unpropped_slabs * manhour_vertical_nonrc_pc) #prepare topping + hoisting
+    
+        slab_hoist_count_tower_crane = math.ceil(no_unpropped_slabs) 
+    
+    if (selected_slab == "2.4HC Slab"):
+        topping_area_slabs = length * width - (formwork_area_beams / 2)
+        formwork_area_slabs = 0
+        slab_rebar_percentage = 1.5
+        weight_slab_rebar = math.ceil((formwork_area_slabs * slab_thickness_m) * 7.85 * slab_rebar_percentage/100)
+        slab_cis_volume = topping_area_slabs * slab_thickness_m
+        
+        # Calculate the number of x positions
+        N_x = int((length + s1 - (s1 * 1.5)) / s1)
+
+        # Calculate the number of y positions
+        N_y = int((width + s2 - (s2 + 2 * column_size_mm)) / s2)
+
+        # Calculate the number of stacked rectangles per position
+        N_stacked = math.floor(s2 / 2.4)
+
+        # Calculate the total number of 1.2HCS
+        no_unpropped_slabs = int(N_x * N_y * N_stacked)
+ 
+        slab_manhours = math.ceil(weight_slab_rebar * manhour_mesh_ton + no_unpropped_slabs * manhour_vertical_nonrc_pc) #prepare topping + hoisting
+    
+        slab_hoist_count_tower_crane = math.ceil(no_unpropped_slabs) 
+    
+    no_propped_slabs = no_propped_slabs + 8
+    weight_loose_rebar = weight_column_rebar + weight_beam_rebar
+    cis_volume = beam_cis_volume + column_cis_volume + slab_cis_volume
+    
+    casting_manhours = math.ceil(cis_volume * manhour_casting_pump_m3)
+    
+    # assign values to utility data
+    hoist_count_passenger_material = 0
+    hoist_count_tower_crane = column_hoist_count_tower_crane + slab_hoist_count_tower_crane + beam_hoist_count_tower_crane
+    no_concrete_truck_deliveries = math.ceil(cis_volume / 8.5)
+    no_trailer_deliveries = math.ceil((no_vertical_pc_com + no_propped_slabs + no_unpropped_slabs)/5 + (weight_beam_rebar + weight_slab_rebar + weight_column_rebar)/15)
+    
+    # assign values to manpower data
+    total_mandays_crane = ((beam_manhours + slab_manhours + column_manhours + casting_manhours)*1.3 +240 + 1800)/ 8 #weather 1.1 manday 8hr safety 1.1 staircase 240 ramp 1400
+    total_productivity_crane = ( (length * width) +600) / total_mandays_crane # ramp 600m2
+    
+    # Design Outputs
+    design_output = [
+    round(no_volumetric_pc_com),
+    round(no_vertical_pc_com),
+    round(no_propped_slabs),
+    round(no_unpropped_slabs),
+    round(formwork_area_slabs),
+    round(formwork_area_beams),
+    round(no_column_formworks),
+    round(weight_loose_rebar),
+    round(weight_slab_rebar),
+    round(cis_volume)
+    ]
+
+    # Equipment Outputs
+    equipment_output = [
+    round(no_tower_cranes),
+    round(no_concrete_pumps),
+    round(no_construction_hoists),
+    round(no_passenger_material_hoists),
+    round(no_gondolas),
+    round(no_mewps)
+    ]
+
+    # Utility Outputs
+    utility_output = [
+    round(hoist_count_passenger_material),
+    round(hoist_count_tower_crane),
+    round(no_concrete_truck_deliveries),
+    round(no_trailer_deliveries)
+    ]
+
+    # Manpower Outputs
+    manpower_output = [
+    round(total_mandays_crane),
+    round(total_productivity_crane,3),
+    round(total_productivity_crane/2,3)
+    ]
 
     return design_output,equipment_output,utility_output,manpower_output, beam_manhours, column_manhours, slab_manhours, casting_manhours
